2ca19b26ab13ca984c3cc1a3a0e906ee96457518
reizo <reizo@TABLET-GLNDV0Q6>
2026-06-02 16:48:31 +0900

    2日目

---
+++ ch03/src/ch03/Main3_7.java
@@ -0,0 +1,12 @@
+package ch03;
+
+public class Main3_7 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+for (int i = 0; i < 7; i++) {
+	System.out.println("こんにちは");
+}
+	}
+
+}
---
+++ ch03/src/ch03/Main3_8.java
@@ -0,0 +1,12 @@
+package ch03;
+
+public class Main3_8 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+for(int i = 0; i < 22; i++) {
+	System.out.print("現在" + (i + 1) + "周目→");
+}
+	}
+
+}
