ef6e28d4db3ff22ea0e62af2b97a87240fa5e075
reizo <reizo@TABLET-GLNDV0Q6>
2026-06-03 14:02:39 +0900

    3日目

--- ch04/src/ch04/Main4_1.java
+++ ch04/src/ch04/Main4_1.java
@@ -2,12 +2,17 @@ package ch04;
 
 public class Main4_1 {
 	public static void main(String[] args) {
-		int sansuu = 20;
+		int sansu = 20;
 		int kokugo = 30;
 		int rika = 40;
 		int eigo = 50;
 		int shakai = 80;
 		
+		int sum = sansu + kokugo + rika + eigo + shakai;
+		
+		int avg = sum / 5;
+		System.out.println("合計点" + sum);
+		System.out.println("平均点" + avg);
 	}
 
 }
