6a0caca5e3be260229c58124f76248d964ebb812
reizo <reizo@TABLET-GLNDV0Q6>
2026-06-03 17:40:06 +0900

    3日目

---
+++ ch04/src/ch04/Main4_2.java
@@ -0,0 +1,11 @@
+package ch04;
+
+public class Main4_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+int [] scores;
+scores = new int[5];
+	}
+
+}
---
+++ ch04/src/ch04/Main4_3.java
@@ -0,0 +1,10 @@
+package ch04;
+
+public class Main4_3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+int[] scores = new int[5];
+	}
+
+}
