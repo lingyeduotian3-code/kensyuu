f48d45820de0de8dabe9df58f761678726339369
reizo <reizo@TABLET-GLNDV0Q6>
2026-06-03 11:13:25 +0900

    3日目

---
+++ ch04/src/ch04/Main4_1.java
@@ -0,0 +1,13 @@
+package ch04;
+
+public class Main4_1 {
+	public static void main(String[] args) {
+		int sansuu = 20;
+		int kokugo = 30;
+		int rika = 40;
+		int eigo = 50;
+		int shakai = 80;
+		
+	}
+
+}
