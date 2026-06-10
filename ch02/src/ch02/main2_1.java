40a5ee846895063fb22fd12c494acecdf321fbe9
reizo <reizo@TABLET-GLNDV0Q6>
2026-06-10 17:58:32 +0900

    8

--- ch02/src/ch02/main2_1.java
+++ ch02/src/ch02/main2_1.java
@@ -4,12 +4,12 @@ public class main2_1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-int a;
-int b;
-a = 20;
-b = a + 5;
-System.out.println(a);
-System.out.println(b);
-}
+		System.out.println("ようこそ、森の館へ");
+		System.out.println("あなたの名前を入力してね");
+		String name = new java.util.Scanner(System.in).nextLine();
+		System.out.println("次はあなたの名前を入力してください");
+		String ageString = new java.util.Scanner(System.in).nextLine(); 
+		int age = Integer.parseInt(ageString); 
+	}
 
 }
