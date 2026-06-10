d7d4e1eefeba67e0989f91ca1aa1557c2159d7d8
reizo <reizo@TABLET-GLNDV0Q6>
2026-06-10 11:51:57 +0900

    8

--- ch11/src/ch11/KyotocleaningShop.java
+++ ch11/src/ch11/KyotocleaningShop.java
@@ -1,5 +1,15 @@
 package ch11;
 
-public class KyotocleaningShop {
+public class KyotocleaningShop implements CleaningService {
+	String ownername;
+	String adres;
+	String phone;
+	public Shirt washShirt(Shirt s) {
+		return s;
+	}public Towl washtowl(Towl t) {
+		return t;
+	}public Coat washcoat(Coat c) {
+		return c;
+	}
 
 }
