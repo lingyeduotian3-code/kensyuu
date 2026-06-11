2b1c66faf49f7d5d5980330174ba585bba87785c
reizo <reizo@TABLET-GLNDV0Q6>
2026-06-11 18:51:19 +0900

    9

--- ch06/src/Calc2.java
+++ ch06/src/Calc2.java
@@ -1,11 +1,11 @@
-import calcapp.logics.CalcLogic;
+import calcapp.main.CalcLogic2;
 
 public class Calc2 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 int a =10; int b =2;
-int total = CalcLogic.tasu(a, b);
-int delta = CalcLogic.hiku(a,b);
+int total = CalcLogic2.tasu(a, b);
+int delta = CalcLogic2.hiku(a,b);
 System.out.println("足すと"+total+"、引くと"+delta);
 }}
--- ch06/src/calcapp/main/Calc2.java
+++ ch06/src/calcapp/main/Calc2.java
@@ -1,5 +1,9 @@
-package calcapp.main;
-
 public class Calc2 {
 
-}
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+int a =10; int b =2;
+int total = CalcLogic2.tasu(a, b);
+int delta = CalcLogic2.hiku(a,b);
+System.out.println("足すと"+total+"、引くと"+delta);
+}}
