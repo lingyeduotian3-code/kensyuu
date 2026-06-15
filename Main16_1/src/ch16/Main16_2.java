9cd7bd16e4bb5eebc4a2751c495331c5b5d6cc25
reizo <reizo@TABLET-GLNDV0Q6>
2026-06-15 18:00:48 +0900

    11

---
+++ Main16_1/src/ch16/Main16_2.java
@@ -0,0 +1,10 @@
+package ch16;
+import java.util.*;
+public class Main16_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+ArrayList<String> names = new ArrayList<String>();
+	}
+
+}
