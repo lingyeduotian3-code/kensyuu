a000331f7f6df717a83839e59697699f8cafb6e9
reizo <reizo@TABLET-GLNDV0Q6>
2026-06-15 15:46:37 +0900

    11

---
+++ ch14/src/ch14/Main14_1.java
@@ -0,0 +1,12 @@
+package ch14;
+
+public class Main14_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Empty14_1 e = new Empty14_1();
+		String s = e.toString();
+		System.out.println(s);
+	}
+
+}
