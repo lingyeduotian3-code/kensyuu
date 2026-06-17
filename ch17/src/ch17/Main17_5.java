91e51bec33372bb866b97bc95e6206c386ea4309
reizo <reizo@TABLET-GLNDV0Q6>
2026-06-17 10:45:19 +0900

    13

---
+++ ch17/src/ch17/Main17_5.java
@@ -0,0 +1,17 @@
+package ch17;
+import java.io.*;
+public class Main17_5 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+try {
+	FileWriter fw = new FileWriter("data.txt");
+	fw.write("hello!");
+}catch(Exception e) {
+	System.out.println("何らかの例外が発生しました");
+}finally {
+	fw.close();
+}
+	}
+
+}
