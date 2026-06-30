8786626522abbd61a45bfdc78a481d247abaadad
reizo <reizo@TABLET-GLNDV0Q6>
2026-06-30 18:01:00 +0900

    2

---
+++ moattravel/src/main/java/com/example/moattravel/entity/VerificationToken.java
@@ -0,0 +1,10 @@
+package com.example.moattravel.entity;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+public class VerificationToken {
+
+}
