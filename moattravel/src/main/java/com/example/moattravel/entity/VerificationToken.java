82c77a9e432df89de5f7b21eb119afa88ca6c4fc
reizo <reizo@TABLET-GLNDV0Q6>
2026-07-01 14:01:37 +0900

    2

--- moattravel/src/main/java/com/example/moattravel/entity/VerificationToken.java
+++ moattravel/src/main/java/com/example/moattravel/entity/VerificationToken.java
@@ -5,6 +5,32 @@ import java.sql.Timestamp;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-public class VerificationToken {
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Entity
+@Table(name = "verification_tokens")
+@Data
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "token")
+    private String token;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at", insertable = false, updatable = false)
+    private Timestamp updatedAt;
 }
