9693526e9226f8a87615c87d1f622e5f57b434b3
Reiya Tada <lingyeduotian3@gmail.com>
2026-07-21 17:47:18 +0900

    reiaut

---
+++ moattravel321/src/main/java/com/example/moattravel321/Moattravel321Application.java
@@ -0,0 +1,13 @@
+package com.example.moattravel321;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moattravel321Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Moattravel321Application.class, args);
+	}
+
+}
